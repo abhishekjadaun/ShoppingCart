09dfc8adb9a2b52590bfdce64478fa4b86bb79f5
singh <singh.787857@yahoo.com>
2021-06-30 23:09:31 +0530

    first commit

---
+++ shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'shop/index.html')
+
+def about(request):
+    return render(request,'shop/index.html')
+
+def contact(request):
+    return render(request,'shop/index.html')
+
+def productview(request):
+    return render(request,'shop/index.html')
+
+def search(request):
+    return render(request,'shop/index.html')
+
+def tracker(request):
+    return render(request,'shop/index.html')
+
+def checkout(request):
+    return render(request,'shop/index.html')
