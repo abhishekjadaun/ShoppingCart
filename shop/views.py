d361a74c6b1284814e1ff2696140c32fdcb67c1b
singh <singh.787857@yahoo.com>
2021-07-03 01:01:07 +0530

    commit Product slide

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,HttpResponse
-
+from shop.models import Product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products= Product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+
+    return render(request,'shop/index.html',params)
 
 def about(request):
     return render(request,'shop/about.html')
