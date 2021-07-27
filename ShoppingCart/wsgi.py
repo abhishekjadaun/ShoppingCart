d01066cc2020f4d23a886e2b8038ede20c0005d4
singh <singh.787857@yahoo.com>
2021-07-27 13:47:54 +0530

    commit

--- ShoppingCart/wsgi.py
+++ ShoppingCart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyAwesomeCart project.
+WSGI config for ShoppingCart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyAwesomeCart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShoppingCart.settings')
 
 application = get_wsgi_application()
